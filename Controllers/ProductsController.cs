82849d4e2474809cd8c62e069316f3d41474e6cd
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2023-03-09 14:45:06 -0500

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GraphQLProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private IProduct _productService;
+        public ProductsController(IProduct productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _productService.GetAllProducts();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return  _productService.GetProductById(id);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public Product Post([FromBody] Product product)
+        {
+           return _productService.AddProduct(product);
+          
+
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public Product Put(int id, [FromBody] Product product)
+        {
+            return _productService.UpdateProduct(id, product);
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _productService.DeleteProduct(id);
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+
+namespace GraphQLProject.Services
+{
+    public class ProductService : IProduct
+    {
+
+        private static List<Product> products = new List<Product>() {
+            new Product { Id = 0, Name = "Coffee" , Price = 10},
+            new Product { Id = 1, Name ="Tea", Price = 15}
+        };
+
+        public Product AddProduct(Product product)
+        {
+            products.Add(product);
+            return product;
+        }
+
+        public void DeleteProduct(int id)
+        {
+            products.RemoveAt(id);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return products.Find(f => f.Id == id);                
+        }
+
+        public Product UpdateProduct(int id, Product product)
+        {
+            products[id] = product;
+            return product;
+        }
+    }
+}
